1a82298a650e67610bfa719ba452f185f2060704
JP Ventura <contact@jpventura.com>
2017-12-17 14:53:26 -0200

    WIP

---
+++ app.py
@@ -0,0 +1,7 @@
+import csv
+from datetime import datetime
+import numpy as np
+import pandas as pd
+from babs_datacheck import question_3
+from babs_visualizations import usage_stats, usage_plot
+from IPython.display import display
